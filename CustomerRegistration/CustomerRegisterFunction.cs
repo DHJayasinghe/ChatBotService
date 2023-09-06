2fb8bdd34e16cc40cf10bc084a1c6563473af9a9
Dhanuka Jayasinghe <dhanuka@jrcsoftware.com>
2023-09-06 19:13:44 +0530

    Added registration validation

--- CustomerRegistration/CustomerRegisterFunction.cs
+++ CustomerRegistration/CustomerRegisterFunction.cs
@@ -2,11 +2,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Microsoft.Graph;
 using Azure.Identity;
 using Microsoft.Graph.Models;
+using Microsoft.Graph.Models.ODataErrors;
 
 namespace CustomerRegistration
 {
@@ -17,26 +17,44 @@ namespace CustomerRegistration
         string clientSecret = "nrs8Q~VcMNNZHrGHQEJbol8cdOpnbCxs9AALIb9h";
 
         [FunctionName("Register")]
-        public async Task<ActionResult> Register([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "Register")] HttpRequest req, ILogger log)
+        public async Task<ActionResult> Register([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "Register")] UserRequest req, ILogger log)
         {
             var clientSecretCredentials = new ClientSecretCredential(tenantId, clientId, clientSecret);
             GraphServiceClient client = new GraphServiceClient(clientSecretCredentials);
 
+
             var requestBody = new User
             {
                 AccountEnabled = true,
-                DisplayName = "Adele Vance",
-                MailNickname = "AdeleV",
-                UserPrincipalName = "AdeleV@iamdhanukagmail.onmicrosoft.com",
+                DisplayName = req.DisplayName,
+                MailNickname = req.MailNickname,
+                UserPrincipalName = req.UserPrincipalName,
                 PasswordProfile = new PasswordProfile
                 {
                     ForceChangePasswordNextSignIn = true,
                     Password = "xWwvJ]6NMw+bWH-d",
                 },
             };
-            var result = await client.Users.PostAsync(requestBody);
+            try
+            {
+                var result = await client.Users.PostAsync(requestBody);
+            }
+            catch (ODataError ex)
+            {
+                if (ex.ResponseStatusCode == (int)System.Net.HttpStatusCode.BadRequest)
+                    return new BadRequestObjectResult(ex.Error.Message);
+                throw;
+            }
+
 
             return new OkResult();
         }
     }
+}
+
+public record UserRequest
+{
+    public string DisplayName { get; init; }
+    public string MailNickname { get; init; }
+    public string UserPrincipalName { get; init; }
 }
