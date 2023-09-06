fe4e3143f2bbbb381af42416e85e87c5ff0c9c59
Dhanuka Jayasinghe <dhanuka@jrcsoftware.com>
2023-09-06 18:54:32 +0530

    Add project files.

---
+++ CustomerRegistration/Company.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomerRegistration;
+public class Company
+{
+    public string id { get; set; }
+    public string phone { get; set; }
+    public string cellphone { get; set; }
+    public string website { get; set; }
+    public string pricingplanid { get; set; }
+    public string companyName { get; set; }
+    public string companyEmail { get; set; }
+    public string address { get; set; }
+    public string city { get; set; }
+    public string state { get; set; }
+    public string zipcode { get; set; }
+    public List<string> additionalPlanmembers { get; set; }
+    public DateTime creationDate { get; set; }
+    public bool success { get; set; } = false;
+    public bool isJobSync { get; set; }
+}
---
+++ CustomerRegistration/Function1.cs
@@ -0,0 +1,43 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Graph;
+using Azure.Identity;
+using Microsoft.Graph.Models;
+using System;
+
+namespace CustomerRegistration
+{
+    public class CRegister
+    {
+        string tenantId = "69380036-9b93-4add-a715-9e8921d06841";
+        string clientId = "97c063f7-39b4-469d-9861-0daf9c08adf2";
+        string clientSecret = "nrs8Q~VcMNNZHrGHQEJbol8cdOpnbCxs9AALIb9h";
+
+        [FunctionName("Register")]
+        public async Task<ActionResult> Register([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "Register")] HttpRequest req, ILogger log)
+        {
+            var clientSecretCredentials = new ClientSecretCredential(tenantId, clientId, clientSecret);
+            GraphServiceClient client = new GraphServiceClient(clientSecretCredentials);
+
+            var requestBody = new User
+            {
+                AccountEnabled = true,
+                DisplayName = "Adele Vance",
+                MailNickname = "AdeleV",
+                UserPrincipalName = "AdeleV@iamdhanukagmail.onmicrosoft.com",
+                PasswordProfile = new PasswordProfile
+                {
+                    ForceChangePasswordNextSignIn = true,
+                    Password = "xWwvJ]6NMw+bWH-d",
+                },
+            };
+            var result = await client.Users.PostAsync(requestBody);
+
+            return new OkResult();
+        }
+    }
+}
